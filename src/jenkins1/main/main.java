b72bcfb0c9d4cb4c9d6c6e2f5837cdc7159135a0
ceci06 <ceci_jbam@hotmail.com>
2019-07-09 21:59:05 -0500

    Initial commit

---
+++ src/jenkins1/main/main.java
@@ -0,0 +1,19 @@
+package jenkins1.main;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		for (int c=0; c<=5; c++) {
+			if (c<5) {
+			System.out.println("Hola Mundo");
+			}
+			else
+			{
+				System.out.println("Tu ciclo finalizó");
+			}
+		}
+	}
+
+}
